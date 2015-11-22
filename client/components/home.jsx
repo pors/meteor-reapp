a79078100bac4e399ac763d01154debfa7b6cfa1
Mark Pors <mark@pors.net>
2015-11-22 22:06:42 +0200

    added reapp-ui + first reapp element (button)

--- client/components/home.jsx
+++ client/components/home.jsx
@@ -1,3 +1,6 @@
+import iOSTheme from 'reapp-ui/themes/ios/theme'
+import Theme from 'reapp-ui/helpers/Theme';
+import Button from 'reapp-ui/components/Button';
 import React, { Component } from 'react';
 
 export default class Home extends Component {
@@ -5,6 +8,9 @@ export default class Home extends Component {
     return (
       <div>
         <h3>Home</h3>
+        <Theme {...iOSTheme}>
+          <Button color="red" />
+        </Theme>
       </div>
     );
   }
